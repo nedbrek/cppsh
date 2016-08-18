c28477e7ad886aa01d1274c09212d357e339a3a2
Edward Brekelbaum <nedbrek@yahoo.com>
2016-08-18 17:59:29 -0500

    Shortened name of toplevel window to .t

--- wcppsh.tcl
+++ wcppsh.tcl
@@ -4,8 +4,8 @@ source cppsh.tcl
 
 proc doRun {} {
 	set d [dict create]
-	dict set d Globals [.tMain.tGlobals get 1.0 end]
-	dict set d Main    [.tMain.tMain    get 1.0 end]
+	dict set d Globals [.t.tGlobals get 1.0 end]
+	dict set d Main    [.t.tMain    get 1.0 end]
 	dict set d Flags   "-std=c++0x"
 
 	set r [runCC $d]
@@ -16,29 +16,29 @@ proc doRun {} {
 
 	set execout [dGet $r POUT]
 	if {$execout ne ""} {
-		.tMain.tOut insert end "$execout\n"
+		.t.tOut insert end "$execout\n"
 	}
 }
 
 ### gui
 wm withdraw .
 
-toplevel .tMain
+toplevel .t
 
 # left side
-pack [frame .tMain.fL] -side left -anchor nw
+pack [frame .t.fL] -side left -anchor nw
 
-pack [label .tMain.lGlobals -text "Global"] -side top -anchor nw -in .tMain.fL
-pack [text  .tMain.tGlobals -height 12] -side top -anchor nw -in .tMain.fL
+pack [label .t.lGlobals -text "Global"] -side top -anchor nw -in .t.fL
+pack [text  .t.tGlobals -height 12] -side top -anchor nw -in .t.fL
 
-pack [label .tMain.lMain -text "Main"] -side top -anchor nw -in .tMain.fL
-pack [text  .tMain.tMain] -side top -anchor nw -in .tMain.fL
+pack [label .t.lMain -text "Main"] -side top -anchor nw -in .t.fL
+pack [text  .t.tMain] -side top -anchor nw -in .t.fL
 
-pack [label .tMain.lOut -text "Output"] -side top -anchor nw -in .tMain.fL
-pack [text  .tMain.tOut] -side top -anchor nw -in .tMain.fL
+pack [label .t.lOut -text "Output"] -side top -anchor nw -in .t.fL
+pack [text  .t.tOut] -side top -anchor nw -in .t.fL
 
 # right side
-pack [frame .tMain.fR] -side left -anchor nw
+pack [frame .t.fR] -side left -anchor nw
 
-pack [button .tMain.bRun -text "Run" -command doRun] -side top -anchor n
+pack [button .t.bRun -text "Run" -command doRun] -side top -anchor n
 
